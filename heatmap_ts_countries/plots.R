6a9efdee7518d0daef5aab0aeb46f4c2475e3190
Matt Brigida <mdbrigida@gmail.com>
2020-07-21 15:17:25 -0400

    updated data and plots

--- heatmap_ts_countries/plots.R
+++ heatmap_ts_countries/plots.R
@@ -9,24 +9,22 @@ african_data$Date <- as.Date(as.character(african_data$Date), format = "%Y-%m-%d
 names(african_data)[4] <- "School Status"
 african_data$y <- african_data$CountryName
 
-
-
-
-
 school_closing_data <- african_data[, names(african_data) %in% c("Date", "CountryName", "School Status")]
 
 school_closing_data <- school_closing_data[complete.cases(school_closing_data), ]
 
-
+school_closing_data$CountryName <- factor(school_closing_data$CountryName,levels=rev(unique(school_closing_data$CountryName)))
 
 ggplot(school_closing_data, aes(x = Date, y = CountryName, fill = `School Status`)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
     geom_tile() +
     scale_fill_gradientn(
-        colours = c("#FF6633", "#FF3333"),#"lightpink1", "indianred1"),
-        limits = c(0, 2),
+        colours = c("gray81", "lightblue1", "#0066CC"),
+#        colours = c("gray81", "lightblue1", "#0066CC", "navyblue"), #j orig
+        limits = c(0, 3),
         oob = squish) +
-    labs(x = "", y = "")
+    labs(x = "", y = "") +
+    scale_x_date(date_breaks = "2 weeks", date_labels = "%b-%d")
 
 
-ggsave("./school_status_heatmap.pdf", width = 15, height = 8.67)
+ggsave("./school_status_heatmap_2.pdf", width = 15, height = 8.67)
