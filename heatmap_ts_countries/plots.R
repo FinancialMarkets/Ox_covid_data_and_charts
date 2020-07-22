83f3b44904c53032147fadc1b527027f0288a9d2
Matt Brigida <mdbrigida@gmail.com>
2020-07-22 11:21:08 -0400

    moved start date to feb 15

--- heatmap_ts_countries/plots.R
+++ heatmap_ts_countries/plots.R
@@ -15,16 +15,18 @@ school_closing_data <- school_closing_data[complete.cases(school_closing_data), 
 
 school_closing_data$CountryName <- factor(school_closing_data$CountryName,levels=rev(unique(school_closing_data$CountryName)))
 
+school_closing_data <- school_closing_data[school_closing_data$Date > "2020-02-14", ]
+
 ggplot(school_closing_data, aes(x = Date, y = CountryName, fill = `School Status`)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
     geom_tile() +
     scale_fill_gradientn(
-        colours = c("gray81", "lightblue1", "#0066CC"),
-#        colours = c("gray81", "lightblue1", "#0066CC", "navyblue"), #j orig
+#        colours = c("gray81", "lightblue1", "#0066CC"),
+        colours = c("gray81", "lightblue1", "#0066CC", "navyblue"), #j orig
         limits = c(0, 3),
         oob = squish) +
     labs(x = "", y = "") +
     scale_x_date(date_breaks = "2 weeks", date_labels = "%b-%d")
 
 
-ggsave("./school_status_heatmap_2.pdf", width = 15, height = 8.67)
+ggsave("./school_status_heatmap.pdf", width = 15, height = 8.67)
